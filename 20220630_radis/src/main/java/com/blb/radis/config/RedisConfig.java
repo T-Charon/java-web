8461be33aac48569fc4863a7544ef2c2decc22d3
Charon <1635576341@qq.com>
2022-06-30 20:32:34 +0800

    简单学习redis数据库的用法

--- 20220630_radis/src/main/java/com/blb/radis/config/RedisConfig.java
+++ 20220630_radis/src/main/java/com/blb/radis/config/RedisConfig.java
@@ -1,8 +1,44 @@
 package com.blb.radis.config;
 
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.PropertyAccessor;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
 /**
  * @Author Charon
  * @Date 2022/6/30
  **/
-public class RedisTemplate {
+@Configuration
+public class RedisConfig {
+
+    @Bean
+    public RedisTemplate<String,Object>redisTemplate(RedisConnectionFactory factory){
+        //创建redisTemplate
+        RedisTemplate<String,Object>redisTemplate = new RedisTemplate<>();
+        //设置连接工厂
+        redisTemplate.setConnectionFactory(factory);
+        //创建json序列化器
+        Jackson2JsonRedisSerializer jackson2JsonRedisSerializer = new Jackson2JsonRedisSerializer(Object.class);
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
+        objectMapper.enableDefaultTyping(ObjectMapper.DefaultTyping.NON_FINAL);
+        jackson2JsonRedisSerializer.setObjectMapper(objectMapper);
+        StringRedisSerializer stringRedisSerializer  = new StringRedisSerializer();
+
+        //keg采用string序列化方式
+        redisTemplate.setKeySerializer(stringRedisSerializer);
+        redisTemplate.setHashKeySerializer(stringRedisSerializer);
+        redisTemplate.setValueSerializer(jackson2JsonRedisSerializer);
+        redisTemplate.setHashValueSerializer(jackson2JsonRedisSerializer);
+        redisTemplate.afterPropertiesSet();
+        return redisTemplate;
+
+
+    }
 }
