cb5bbad47d7b1baa2c6175dd2d1c615bf76fcd25
Charon <1635576341@qq.com>
2022-08-15 16:42:47 +0800

    将Eureka+Config+Rabbitmq改为使用Nacos完成注册+拉取配置

--- 20220808_springcloud/mysql_product_service/src/main/java/com/blb/mysql_product_service/mapper/ProductMapper.java
+++ 20220808_springcloud/mysql_product_service/src/main/java/com/blb/mysql_product_service/mapper/ProductMapper.java
@@ -3,10 +3,12 @@ package com.blb.mysql_product_service.mapper;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.blb.comment.entity.Product;
+import org.apache.ibatis.annotations.Mapper;
 
 /**
  * @Entity com.blb.mysql_product_service.entity.TProduct
  */
+@Mapper
 public interface ProductMapper extends BaseMapper<Product> {
 
     IPage<Product> getList(IPage page);
